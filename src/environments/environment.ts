91d24e0739e6bebbcea2d9b2f4a2b9206827ed94
Mauricio Molina <molinamau37@gmail.com>
2025-06-18 10:30:27 -0500

    adding enviroment for database in firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDw727xJnDqLgRwWzxRH0Zus7PUcEWUXxE',
+    authDomain: 'mercadatos-5b26e.firebaseapp.com',
+    projectId: 'mercadatos-5b26e',
+    storageBucket: 'mercadatos-5b26e.firebasestorage.app',
+    messagingSenderId: '545570077722',
+    appId: '1:545570077722:web:bd5f5acf9618929efa25a3',
+  },
+};
